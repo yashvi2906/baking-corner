e4435664102c528debaf0c294d4df2017ba37fda
yashvi2906 <ryashvih@gmail.com>
2026-05-25 16:09:56 +0530

    Fix asset paths

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/baking-corner/',
-})
+});
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/baking-corner/',
+// })
